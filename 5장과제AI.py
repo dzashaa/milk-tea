6047fe0f937cfb4dbda9e1d65555d58a4843d1f0
dzashaa <dzikris.amaliah@gmail.com>
2024-04-03 20:55:28 +0900

    5장 과제

---
+++ 5장과제AI.py
@@ -0,0 +1,94 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/list_operation_demo.py
+10
+60
+30
+60
+70
+50
+70
+90
+20
+7
+
+==================== RESTART: C:/김아메/AI/과제/multiply_list.py ====================
+3 * 2 = 6
+3 * 3 = 9
+3 * 4 = 12
+3 * 5 = 15
+3 * 6 = 18
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+7 * 2 = 14
+7 * 3 = 21
+7 * 4 = 28
+7 * 5 = 35
+7 * 6 = 42
+
+===================== RESTART: C:/김아메/AI/과제/combine_list.py ====================
+I like pancakes.
+I like kiwi juice.
+I like espresso.
+I love pancakes.
+I love kiwi juice.
+I love espresso.
+
+================== RESTART: C:/김아메/AI/과제/calculate_list_sum.py =================
+리스트 원소들: [10, 20, 30, 50, 60]
+리스트 원소들의 합: 170
+
+==================== RESTART: C:/김아메/AI/과제/find_max_value.py ===================
+리스트 원소들: [10, 20, 30, 50, 60]
+리스트 원소들 중 최댓값: 60
+
+================= RESTART: C:/김아메/AI/과제/calculate_statistics.py ================
+5개의 수를 입력하세요: 45 67 20 34 2
+합 : 168
+평균 : 33.6
+최댓값: 67
+최솟값: 2
+
+================ RESTART: C:/김아메/AI/과제/calculate_meanand_std.py ================
+10개의 수를 입력하세요: 45 67 20 34 100 2 23 45 67 89
+합 : 492
+평균 : 49.2
+표준편차 : 29.71800800861323
+
+==================== RESTART: C:/김아메/AI/과제/string_slicing.py ===================
+Have
+beautiful
+day.
+>>> 
+==================== RESTART: C:/김아메/AI/과제/create_animals.py ===================
+animals = ['dog', 'cat', 'tiger', 'lion']
+>>> 
+==================== RESTART: C:/김아메/AI/과제/create_animals.py ===================
+animals = ['dog', 'cat', 'tiger', 'lion']
+animals = ['cat', 'tiger', 'lion', 'dog']
+animals = ['cat', 'tiger', 'lion', 'dog']
+I love cat.
+I love tiger.
+I love lion.
+I love dog.
+>>> 
+================== RESTART: C:/김아메/AI/과제/remove_duplicates.py ==================
+s_list = ['abc', 'bcd', 'abc', 'abba', 'cddc', 'opq', 'opq']
+new_s_list = ['abc', 'bcd', 'abba', 'cddc', 'opq']
+>>> 
+================== RESTART: C:/김아메/AI/과제/decompress_string.py ==================
+src = a2b3c6a2c3f1g1
+output = aabbbccccccaacccfg
+>>> 
+====== RESTART: C:/Users/진한섭/AppData/Local/Programs/Python/Python312/a.py ======
+4명의 정보가 담겨 있습니다.
+평균 나이는 26.75세입니다.
+리스트에는 남자가 3 명, 여자가 1 명입니다.
+['온달', 20, 1, 180.0, 100.0]
+['이사부', 25, 1, 170.0, 70.0]
+['평강', 22, 0, 169.0, 60.0]
+['혁거세', 40, 1, 150.0, 50.0]
